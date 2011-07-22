e659a17bee5d9d7b063fde8e8aed00b83684fef3
milki <milki@rescomp.berkeley.edu>
2011-07-22 12:30:43 -0700

    Add base from dpaste.de and change urls

--- dpaste/urls.py
+++ dpaste/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls.defaults import patterns, url
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^$', 'dpaste.views.snippet_new', name='snippet_new'),
+    url(r'^$', 'dpaste.views.snippet_userlist', name='snippet_userlist'),
     url(r'^guess/$', 'dpaste.views.guess_lexer', name='snippet_guess_lexer'),
     url(r'^diff/$', 'dpaste.views.snippet_diff', name='snippet_diff'),
-    url(r'^your-latest/$', 'dpaste.views.snippet_userlist', name='snippet_userlist'),
     url(r'^your-settings/$', 'dpaste.views.userprefs', name='snippet_userprefs'),
     url(r'^(?P<snippet_id>[a-zA-Z0-9]{4})/$', 'dpaste.views.snippet_details', name='snippet_details'),
     url(r'^(?P<snippet_id>[a-zA-Z0-9]{4})/delete/$', 'dpaste.views.snippet_delete', name='snippet_delete'),
