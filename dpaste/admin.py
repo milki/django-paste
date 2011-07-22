b03d7fa427783c6067266e0b5e77477ba2e7070c
milki <milki@rescomp.berkeley.edu>
2011-07-22 12:31:10 -0700

    Add import_snippets command

--- dpaste/admin.py
+++ dpaste/admin.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 class SnippetAdmin(admin.ModelAdmin):
     list_display = (
         '__unicode__',
-        'author',
         'lexer',
-        'published',
-        'expires',
     )
 
 admin.site.register(Snippet, SnippetAdmin)
---
+++ dpaste/management/commands/import_snippets.py
@@ -0,0 +1,33 @@
+import datetime
+import sys
+from optparse import make_option
+from django.core.management.base import CommandError, LabelCommand
+from dpaste.models import Snippet
+
+from dulwich.repo import Repo
+from dulwich.object_store import tree_lookup_path
+from dulwich.objects import Blob
+
+class Command(LabelCommand):
+    option_list = LabelCommand.option_list + (
+        make_option('--dry-run', '-d', action='store_true', dest='dry_run', 
+            help='Don\'t do anything.'),
+    )
+    help = "Import snippets"
+
+    def extract_log(x): return re.sub(r".*/","#",x)
+
+    def handle(self, *args, **options):
+        repo = Repo('/home/milki/git/ircmylife-logs.git')
+        allbranches = list(repo.refs.keys())
+        allbranches.sort()
+        branches = allbranches[1:-1]
+
+        new_snippets = [ Snippet.objects.create(branch="%s" % (branch_ref) ) for branch_ref in branches ]
+        sys.stdout.write(u"%s snippets created:\n" % len(new_snippets))
+        for c in new_snippets:
+            sys.stdout.write(u"%s - %s\n" % (c.branch,c.title))
+        if options.get('dry_run'):
+            sys.stdout.write(u'Dry run - Doing nothing! *crossingfingers*\n')
+        else:
+            [ snippet.save() for snippet in new_snippets ]
